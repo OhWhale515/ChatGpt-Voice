4ba752da18e97cfdda0a09c23cbda24b162b43ac
OhWhale515 <sterlinggreen515@gmail.com>
2023-04-11 10:35:09 -0400

    incorp audio-to-text feature

--- ChatGPT-Voice.py
+++ ChatGPT-Voice.py
@@ -9,3 +9,11 @@ openai.api_key = "Insert-API-Key"
 # text-to-speech engine
 engine = pyttsx3.init()
 
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print("Skipping unknown error")
