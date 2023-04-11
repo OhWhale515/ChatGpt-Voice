73e92952a359edd49211fa92605f8839d6441a68
OhWhale515 <sterlinggreen515@gmail.com>
2023-04-11 10:19:55 -0400

    imports and engine

---
+++ ChatGPT-Voice.py
@@ -0,0 +1,11 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+import time
+
+# set OpenAI API key
+openai.api_key = "Insert-API-Key"
+
+# text-to-speech engine
+engine = pyttsx3.init()
+
